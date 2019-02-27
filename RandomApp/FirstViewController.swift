48d4ae52757b19f9089bfea4dfe718cd4e36c772
Student <student@ch140024m.nwmissouri.edu>
2019-02-27 16:35:53 -0600

    Initial Commit

---
+++ RandomApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RandomApp
+//
+//  Created by Student on 2/27/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RandomApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RandomApp
+//
+//  Created by Student on 2/27/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
